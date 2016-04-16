03fc0bd9fe767bce876bc2cf6159851b5a430f73
seesharpcode <seesharpcode@gmail.com>
2016-04-16 13:57:54 -0500

    removing old WPF project

--- ENBOrganizer.App/ViewModels/PresetsOverviewViewModel.cs
+++ ENBOrganizer.App/ViewModels/PresetsOverviewViewModel.cs
@@ -4,9 +4,9 @@ using ENBOrganizer.Util;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
+using System.Windows.Input;
 
 namespace ENBOrganizer.App.ViewModels
 {
@@ -15,14 +15,89 @@ namespace ENBOrganizer.App.ViewModels
         private readonly PresetService _presetService;
         private readonly GameService _gameService;
 
+        private readonly ICommand _addBlankPresetCommand;
+        private readonly ICommand _importFolderCommand;
+        private readonly ICommand _importArchiveCommand;
+        private readonly ICommand _importActiveFilesCommand;
+
         public ObservableCollection<Preset> Presets { get; set; }
+        public List<TitledCommand> TitledCommands { get; set; }
 
         public PresetsOverviewViewModel(PresetService presetService, GameService gameService)
         {
             _presetService = presetService;
             _gameService = gameService;
 
+            _addBlankPresetCommand = new ActionCommand(AddBlank, () => true);
+            _importFolderCommand = new ActionCommand(ImportFolder, () => true);
+            _importArchiveCommand = new ActionCommand(ImportArchive, () => true);
+            _importActiveFilesCommand = new ActionCommand(ImportActiveFiles, () => true);
+
+            TitledCommands = new List<TitledCommand>
+            {
+                new TitledCommand("Blank", "Create a blank preset", _addBlankPresetCommand),
+                new TitledCommand("Import Folder", "Create a preset from a folder", _importFolderCommand),
+                new TitledCommand("Import Archive", "Create a preset an archive (.zip, .7z)", _importArchiveCommand),
+                new TitledCommand("Import Active Files", "Create a preset from preset files/folders currently in your game folder", _importActiveFilesCommand)
+            };
+
             Presets = _presetService.GetByGame(_gameService.CurrentGame).ToObservableCollection();
+        }
+
+        private async void AddBlank()
+        {
+            // TODO: exception handling
+            string name = await DialogService.ShowInputDialog("Add Blank Preset", "Please enter a name for your preset:");
+
+            _presetService.Add(new Preset(name, _gameService.CurrentGame));
+        }
+
+        private async void ImportActiveFiles()
+        {
+            // TODO: exception handling
+            string name = await DialogService.ShowInputDialog("Import Active Files", "Please enter a name for your preset:");
+
+            _presetService.ImportActiveFiles(new Preset(name, _gameService.CurrentGame));
+        }
+
+        private void ImportArchive()
+        {
+            string archivePath = DialogService.ShowOpenFileDialog("Please select a .zip file", "ZIP Files(*.zip) | *.zip");
+
+            if (archivePath == string.Empty)
+                return;
+
+            _presetService.ImportArchive(archivePath, _gameService.CurrentGame);
+        }
+
+        private void ImportFolder()
+        {
+            FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog { Description = "Please select the preset folder..." };
+
+            // TODO: let DialogService handle showing this dialog
+            if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    _presetService.ImportFolder(folderBrowserDialog.SelectedPath, _gameService.CurrentGame);
+                }
+                catch (InvalidOperationException exception)
+                {
+                    // TODO: MessageBoxUtil.ShowError(exception.Message);
+                }
+                catch (UnauthorizedAccessException exception)
+                {
+                    //MessageBoxUtil.ShowError("Not all files were imported." + Environment.NewLine + Environment.NewLine + exception.Message);
+                }
+                catch (PathTooLongException exception)
+                {
+                    //MessageBoxUtil.ShowError(exception.Message);
+                }
+                catch (IOException exception)
+                {
+                    //MessageBoxUtil.ShowError("Not all files were imported." + Environment.NewLine + Environment.NewLine + exception.Message);
+                }
+            }
         }
     }
 }
--- ENBOrganizer.App/ViewModels/ViewModelLocator.cs
+++ ENBOrganizer.App/ViewModels/ViewModelLocator.cs
@@ -12,14 +12,12 @@ namespace ENBOrganizer.App.ViewModels
 
         public static GameService GameService { get { return _gameService; } }
         
-        private readonly static PresetsViewModel _presetsViewModel = new PresetsViewModel(_presetService, GameService, _presetItemsService);
         private readonly static GamesViewModel _gamesViewModel = new GamesViewModel(GameService);
         private readonly static MainViewModel _mainViewModel = new MainViewModel(_gamesViewModel);
         private readonly static AddGameViewModel _addGameViewModel = new AddGameViewModel(GameService, _gamesViewModel);
         private readonly static PresetsOverviewViewModel _presetsOverviewViewModel = new PresetsOverviewViewModel(_presetService, GameService);
         private readonly static PresetDetailViewModel _presetDetailViewModel = new PresetDetailViewModel(_presetItemsService);
-
-        public static PresetsViewModel PresetsViewModel { get { return _presetsViewModel; } }
+        
         public static GamesViewModel GamesViewModel { get { return _gamesViewModel; } }
         public static MainViewModel MainViewModel { get { return _mainViewModel; } }
         public static AddGameViewModel AddGameViewModel { get { return _addGameViewModel; } }

3cb9c3b3aa0d7feec927a7cc6d91eba22443482e
seesharpcode <seesharpcode@gmail.com>
2016-04-19 22:03:56 -0500

    tile image creation is working

---
+++ ENBOrganizer.App/Converters/PathToBitmapConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media.Imaging;
+
+namespace ENBOrganizer.App.Converters
+{
+    public class PathToBitmapConverter : IValueConverter
+    {
+        // TODO: this could be more elegant
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            try
+            {
+                string imagePath = value.ToString();
+
+                return new BitmapImage(new Uri(imagePath)) { CacheOption = BitmapCacheOption.OnLoad };
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+            
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

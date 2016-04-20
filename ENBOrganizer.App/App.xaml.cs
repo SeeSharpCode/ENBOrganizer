eeffe5cab38454ec5d66818052a436aae983d77a
seesharpcode <seesharpcode@gmail.com>
2016-04-19 19:02:02 -0500

    mostly formatting changes

--- ENBOrganizer.App/App.xaml.cs
+++ ENBOrganizer.App/App.xaml.cs
@@ -5,13 +5,13 @@ namespace ENBOrganizer.App
     // TODO: lowercase XAML namespace aliases
     // TODO: indeterminate progress bar on footer
     // TODO: tooltips for everything
-    // TODO: Win10 style on enabled/disabled
-    // TODO: notification that masterlist items are added upon adding preset
     // TODO: double-check binding modes
     // TODO: remove margin/padding from labels
     // TODO: title bar toggle buttons
     // TODO: fix context menu on files treeview
     // TODO: remove unused icons
+    // TODO: split PresetItems into its own view model?
+    // TODO: standardize font sizes
     public partial class App : Application
     {
     }

1b85d3ac3b4a65b0689e9969b6c66c5c6d90eef8
SeeSharpCode <seesharpcode@gmail.com>
2016-04-27 19:59:53 -0500

    removing cleanup.cmd

--- Cleanup.cmd
+++
@@ -1,4 +0,0 @@
-RD /S /Q "D:\Documents\Visual Studio 2015\Projects\ENBOrganizer\ENBOrganizer.UI\bin\Debug\Games"
-RD /S /Q "C:\Users\auntb\AppData\Local\BluShark_Media\ENBOrganizer.vshost.*"
-Del "D:\Documents\Visual Studio 2015\Projects\ENBOrganizer\ENBOrganizer.UI\bin\Debug\Games.xml"
-Del "D:\Documents\Visual Studio 2015\Projects\ENBOrganizer\ENBOrganizer.UI\bin\Debug\Presets.xml"

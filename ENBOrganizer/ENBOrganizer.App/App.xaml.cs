64f816aba5c06ddf00ca827063244a4b6c857914
SeeSharpCode <seesharpcode@gmail.com>
2016-05-12 11:35:17 -0500

    updated TODO list

--- ENBOrganizer/ENBOrganizer.App/App.xaml.cs
+++ ENBOrganizer/ENBOrganizer.App/App.xaml.cs
@@ -6,10 +6,7 @@ namespace ENBOrganizer.App
     // TODO: indeterminate progress bar on footer
     // TODO: tooltips for everything
     // TODO: double-check binding modes
-    // TODO: title bar toggle buttons
-    // TODO: ContextMenu on TreeViewItems only works when right-clicking on text
     // TODO: remove unused icons
-    // TODO: split PresetItems into its own view model?
     // TODO: double-check reponsibilties of services
     // TODO: optimize UI speed
     // TODO: settings

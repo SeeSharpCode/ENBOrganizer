be762b3f75c383294c530b6ff988e8e9706a014b
seesharpcode <seesharpcode@gmail.com>
2016-05-12 22:11:52 -0500

    moved TODO list to GitHub

--- ENBOrganizer/ENBOrganizer.App/App.xaml.cs
+++ ENBOrganizer/ENBOrganizer.App/App.xaml.cs
@@ -3,20 +3,7 @@ using System.Windows;
 
 namespace ENBOrganizer.App
 {
-    // TODO: indeterminate progress bar on footer
-    // TODO: remove unused icons
-    // TODO: optimize UI speed
-    // TODO: settings
-    // TODO: add support for binaries
-    // TODO: save the user's last tab
-    // TODO: icon
-    // TODO: essential files/folders
-    // TODO: how to setup Games folder?
-    // TODO: n00b's guide
-    // TODO: drag and drop
-    // TODO: recognize changes made outside the program
     // TODO: Remove exception handling when it's just catching Exception and displaying a message. This could be done globally.
-
     // TODO: better exception dialog in here
     public partial class App : Application
     {

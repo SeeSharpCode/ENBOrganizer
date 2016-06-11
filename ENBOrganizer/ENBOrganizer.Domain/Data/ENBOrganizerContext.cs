a0bc629f98ba1992f18990b6cc451e654854eb50
seesharpcode <seesharpcode@gmail.com>
2016-06-11 03:09:31 -0500

    added EF context

--- ENBOrganizer/ENBOrganizer.Domain/Data/ENBOrganizerContext.cs
+++ ENBOrganizer/ENBOrganizer.Domain/Data/ENBOrganizerContext.cs
@@ -1,14 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using ENBOrganizer.Domain.Entities;
+using SQLite.CodeFirst;
 using System.Data.Entity;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ENBOrganizer.Domain.Data
 {
     public class ENBOrganizerContext : DbContext
     {
+        public DbSet<Game> Games { get; set; }
+        public DbSet<Binary> Binaries { get; set; }
+        public DbSet<Preset> Presets { get; set; }
+        public DbSet<MasterListItem> MasterListItem { get; set; }
 
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            SqliteCreateDatabaseIfNotExists<ENBOrganizerContext> sqlLiteConnectionInitializer = new SqliteCreateDatabaseIfNotExists<ENBOrganizerContext>(modelBuilder);
+            Database.SetInitializer(sqlLiteConnectionInitializer);
+        }
     }
 }
